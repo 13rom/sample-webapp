2389daaecfd8116d6b214f39fd9e357037a1171f
Oleg Romanov <mailtome@ua.fm>
2023-02-01 16:54:49 +0200

    Add compilation stage

--- springbootapp/src/main/java/com/example/demo/FinalProjectDemoApplication.java
+++ springbootapp/src/main/java/com/example/demo/FinalProjectDemoApplication.java
@@ -3,7 +3,6 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -15,8 +14,8 @@ public class FinalProjectDemoApplication {
 	}
 	
 	@GetMapping("/")
-	public String hello(@RequestParam(value = "name", defaultValue = "and peace for World!") String name) {
-	return String.format("Hello %s!", name);
-	}
+    public String hello() {
+        return "SpringBoot Hello World! \nJenkins build #%BUILD% (%DATE%)";
+    }
 
 }
